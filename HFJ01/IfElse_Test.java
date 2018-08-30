9df6acce5583fea867b8cadbf20030c4d63b66ca
ShamanArena <919639502@qq.com>
2018-08-30 10:57:37 +0800

    第一章练习

---
+++ HFJ01/IfElse_Test.java
@@ -0,0 +1,24 @@
+public class IfElse_Test
+{
+	public static void main(String[] args)
+	{
+		int x = 10;
+		if (x == 10)
+     	{
+			System.out.print("x must be 10");
+		}
+		else
+		{
+			System.out.print("x isn't 10");
+		} 
+		
+		/*
+		if ((x < 3) & (name.equals("Dirk")))
+		{
+			System .out.println("Gently");
+		}
+		*/
+		System.out.print("this line run no matter what");
+		
+	}
+}
---
+++ HFJ01/LoopTest.java
@@ -0,0 +1,11 @@
+public class LoopTest
+{
+	public static void main(String[] args)
+	{
+	    
+		for (int x = 0;x < 10;x = x + 1)
+		{
+			System.out.print("x is now " + x +";");
+		}
+	}
+}
